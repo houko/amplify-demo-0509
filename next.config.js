ff25d76ab09f256f7059e4006a32435b4207278b
Evan <suzukaze.hazuki2020@gmail.com>
2022-05-09 22:52:49 +0900

    feat: fix export error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: ''
+  }
 }
-
-module.exports = nextConfig
